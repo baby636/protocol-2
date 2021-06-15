cece-z/protocol

`@0x/asset-swapper`: eth_call Refactors

packages/asset-swapper/contracts/src/uniswapv3/UniswapV3.sol
@@ -1,0 +1,49 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+
+  Copyright 2021 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.6;
+pragma experimental ABIEncoderV2;
+
+import "@0x/contracts-erc20/contracts/src/v06/IERC20TokenV06.sol";
+
+interface IUniswapV3Quoter {
+    function factory()
+        external
+        view
+        returns (IUniswapV3Factory factory);
+    function quoteExactInput(bytes memory path, uint256 amountIn)
+        external
+        returns (uint256 amountOut);
+    function quoteExactOutput(bytes memory path, uint256 amountOut)
+        external
+        returns (uint256 amountIn);
+}
+
+interface IUniswapV3Factory {
+    function getPool(IERC20TokenV06 a, IERC20TokenV06 b, uint24 fee)
+        external
+        view
+        returns (IUniswapV3Pool pool);
+}
+
+interface IUniswapV3Pool {
+    function token0() external view returns (IERC20TokenV06);
+    function token1() external view returns (IERC20TokenV06);
+    function fee() external view returns (uint24);
+}

packages/asset-swapper/test/artifacts.ts
@@ -10,6 +10,7 @@ import * as BalanceChecker from '../test/generated-artifacts/BalanceChecker.json
 import * as BalancerSampler from '../test/generated-artifacts/BalancerSampler.json';
 import * as BalancerV2Sampler from '../test/generated-artifacts/BalancerV2Sampler.json';
 import * as BancorSampler from '../test/generated-artifacts/BancorSampler.json';
+import * as CallDispatcher from '../test/generated-artifacts/CallDispatcher.json';
 import * as CurveSampler from '../test/generated-artifacts/CurveSampler.json';
 import * as DODOSampler from '../test/generated-artifacts/DODOSampler.json';
 import * as DODOV2Sampler from '../test/generated-artifacts/DODOV2Sampler.json';
@@ -45,7 +46,9 @@ import * as TestNativeOrderSampler from '../test/generated-artifacts/TestNativeO
 import * as TwoHopSampler from '../test/generated-artifacts/TwoHopSampler.json';
 import * as UniswapSampler from '../test/generated-artifacts/UniswapSampler.json';
 import * as UniswapV2Sampler from '../test/generated-artifacts/UniswapV2Sampler.json';
-import * as UniswapV3Sampler from '../test/generated-artifacts/UniswapV3Sampler.json';
+import * as UniswapV3BuySampler from '../test/generated-artifacts/UniswapV3BuySampler.json';
+import * as UniswapV3Common from '../test/generated-artifacts/UniswapV3Common.json';
+import * as UniswapV3SellSampler from '../test/generated-artifacts/UniswapV3SellSampler.json';
 import * as UtilitySampler from '../test/generated-artifacts/UtilitySampler.json';
 export const artifacts = {
     ApproximateBuys: ApproximateBuys as ContractArtifact,
@@ -53,6 +56,7 @@ export const artifacts = {
     BalancerSampler: BalancerSampler as ContractArtifact,
     BalancerV2Sampler: BalancerV2Sampler as ContractArtifact,
     BancorSampler: BancorSampler as ContractArtifact,
+    CallDispatcher: CallDispatcher as ContractArtifact,
     CurveSampler: CurveSampler as ContractArtifact,
     DODOSampler: DODOSampler as ContractArtifact,
     DODOV2Sampler: DODOV2Sampler as ContractArtifact,
@@ -73,7 +77,9 @@ export const artifacts = {
     TwoHopSampler: TwoHopSampler as ContractArtifact,
     UniswapSampler: UniswapSampler as ContractArtifact,
     UniswapV2Sampler: UniswapV2Sampler as ContractArtifact,
-    UniswapV3Sampler: UniswapV3Sampler as ContractArtifact,
+    UniswapV3BuySampler: UniswapV3BuySampler as ContractArtifact,
+    UniswapV3Common: UniswapV3Common as ContractArtifact,
+    UniswapV3SellSampler: UniswapV3SellSampler as ContractArtifact,
     UtilitySampler: UtilitySampler as ContractArtifact,
     IBalancer: IBalancer as ContractArtifact,
     IBancor: IBancor as ContractArtifact,

packages/asset-swapper/test/wrappers.ts
@@ -8,6 +8,7 @@ export * from '../test/generated-wrappers/balance_checker';
 export * from '../test/generated-wrappers/balancer_sampler';
 export * from '../test/generated-wrappers/balancer_v2_sampler';
 export * from '../test/generated-wrappers/bancor_sampler';
+export * from '../test/generated-wrappers/call_dispatcher';
 export * from '../test/generated-wrappers/curve_sampler';
 export * from '../test/generated-wrappers/d_o_d_o_sampler';
 export * from '../test/generated-wrappers/d_o_d_o_v2_sampler';
@@ -43,5 +44,7 @@ export * from '../test/generated-wrappers/test_native_order_sampler';
 export * from '../test/generated-wrappers/two_hop_sampler';
 export * from '../test/generated-wrappers/uniswap_sampler';
 export * from '../test/generated-wrappers/uniswap_v2_sampler';
-export * from '../test/generated-wrappers/uniswap_v3_sampler';
+export * from '../test/generated-wrappers/uniswap_v3_buy_sampler';
+export * from '../test/generated-wrappers/uniswap_v3_common';
+export * from '../test/generated-wrappers/uniswap_v3_sell_sampler';
 export * from '../test/generated-wrappers/utility_sampler';

baby636/protocol-2

packages/asset-swapper/contracts/src/uniswapv3/UniswapV3.sol
@@ -1,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+
+  Copyright 2021 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.6;
+pragma experimental ABIEncoderV2;
+
+import "@0x/contracts-erc20/contracts/src/v06/IERC20TokenV06.sol";
+
+interface IUniswapV3Quoter {
+    function factory()
+        external
+        view
+        returns (IUniswapV3Factory factory_);
+    function quoteExactInput(bytes memory path, uint256 amountIn)
+        external
+        returns (uint256 amountOut);
+    function quoteExactOutput(bytes memory path, uint256 amountOut)
+        external
+        returns (uint256 amountIn);
+}
+
+interface IUniswapV3Factory {
+    function getPool(IERC20TokenV06 a, IERC20TokenV06 b, uint24 fee)
+        external
+        view
+        returns (IUniswapV3Pool pool);
+}
+
+interface IUniswapV3Pool {
+    function token0() external view returns (IERC20TokenV06);
+    function token1() external view returns (IERC20TokenV06);
+    function fee() external view returns (uint24);
+}
+
+// HACK: Keep the compiler from complaining about filenames.
+contract UniswapV3 {}

packages/asset-swapper/test/artifacts.ts
@@ -10,6 +10,7 @@ import * as BalanceChecker from '../test/generated-artifacts/BalanceChecker.json
 import * as BalancerSampler from '../test/generated-artifacts/BalancerSampler.json';
 import * as BalancerV2Sampler from '../test/generated-artifacts/BalancerV2Sampler.json';
 import * as BancorSampler from '../test/generated-artifacts/BancorSampler.json';
+import * as CallDispatcher from '../test/generated-artifacts/CallDispatcher.json';
 import * as CurveSampler from '../test/generated-artifacts/CurveSampler.json';
 import * as DODOSampler from '../test/generated-artifacts/DODOSampler.json';
 import * as DODOV2Sampler from '../test/generated-artifacts/DODOV2Sampler.json';
@@ -45,7 +46,10 @@ import * as TestNativeOrderSampler from '../test/generated-artifacts/TestNativeO
 import * as TwoHopSampler from '../test/generated-artifacts/TwoHopSampler.json';
 import * as UniswapSampler from '../test/generated-artifacts/UniswapSampler.json';
 import * as UniswapV2Sampler from '../test/generated-artifacts/UniswapV2Sampler.json';
-import * as UniswapV3Sampler from '../test/generated-artifacts/UniswapV3Sampler.json';
+import * as UniswapV3 from '../test/generated-artifacts/UniswapV3.json';
+import * as UniswapV3BuySampler from '../test/generated-artifacts/UniswapV3BuySampler.json';
+import * as UniswapV3SamplerCommon from '../test/generated-artifacts/UniswapV3SamplerCommon.json';
+import * as UniswapV3SellSampler from '../test/generated-artifacts/UniswapV3SellSampler.json';
 import * as UtilitySampler from '../test/generated-artifacts/UtilitySampler.json';
 export const artifacts = {
     ApproximateBuys: ApproximateBuys as ContractArtifact,
@@ -53,6 +57,7 @@ export const artifacts = {
     BalancerSampler: BalancerSampler as ContractArtifact,
     BalancerV2Sampler: BalancerV2Sampler as ContractArtifact,
     BancorSampler: BancorSampler as ContractArtifact,
+    CallDispatcher: CallDispatcher as ContractArtifact,
     CurveSampler: CurveSampler as ContractArtifact,
     DODOSampler: DODOSampler as ContractArtifact,
     DODOV2Sampler: DODOV2Sampler as ContractArtifact,
@@ -73,7 +78,6 @@ export const artifacts = {
     TwoHopSampler: TwoHopSampler as ContractArtifact,
     UniswapSampler: UniswapSampler as ContractArtifact,
     UniswapV2Sampler: UniswapV2Sampler as ContractArtifact,
-    UniswapV3Sampler: UniswapV3Sampler as ContractArtifact,
     UtilitySampler: UtilitySampler as ContractArtifact,
     IBalancer: IBalancer as ContractArtifact,
     IBancor: IBancor as ContractArtifact,
@@ -87,6 +91,10 @@ export const artifacts = {
     ISmoothy: ISmoothy as ContractArtifact,
     IUniswapExchangeQuotes: IUniswapExchangeQuotes as ContractArtifact,
     IUniswapV2Router01: IUniswapV2Router01 as ContractArtifact,
+    UniswapV3: UniswapV3 as ContractArtifact,
+    UniswapV3BuySampler: UniswapV3BuySampler as ContractArtifact,
+    UniswapV3SamplerCommon: UniswapV3SamplerCommon as ContractArtifact,
+    UniswapV3SellSampler: UniswapV3SellSampler as ContractArtifact,
     DummyLiquidityProvider: DummyLiquidityProvider as ContractArtifact,
     TestERC20BridgeSampler: TestERC20BridgeSampler as ContractArtifact,
     TestNativeOrderSampler: TestNativeOrderSampler as ContractArtifact,

packages/asset-swapper/test/wrappers.ts
@@ -8,6 +8,7 @@ export * from '../test/generated-wrappers/balance_checker';
 export * from '../test/generated-wrappers/balancer_sampler';
 export * from '../test/generated-wrappers/balancer_v2_sampler';
 export * from '../test/generated-wrappers/bancor_sampler';
+export * from '../test/generated-wrappers/call_dispatcher';
 export * from '../test/generated-wrappers/curve_sampler';
 export * from '../test/generated-wrappers/d_o_d_o_sampler';
 export * from '../test/generated-wrappers/d_o_d_o_v2_sampler';
@@ -43,5 +44,8 @@ export * from '../test/generated-wrappers/test_native_order_sampler';
 export * from '../test/generated-wrappers/two_hop_sampler';
 export * from '../test/generated-wrappers/uniswap_sampler';
 export * from '../test/generated-wrappers/uniswap_v2_sampler';
-export * from '../test/generated-wrappers/uniswap_v3_sampler';
+export * from '../test/generated-wrappers/uniswap_v3';
+export * from '../test/generated-wrappers/uniswap_v3_buy_sampler';
+export * from '../test/generated-wrappers/uniswap_v3_sampler_common';
+export * from '../test/generated-wrappers/uniswap_v3_sell_sampler';
 export * from '../test/generated-wrappers/utility_sampler';